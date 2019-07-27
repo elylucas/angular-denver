750f34dd7d57f6cb2b5e0552c30cd26412620ed5
Ely Lucas <ely@meta-tek.net>
2019-07-27 08:14:13 -0600

    pwa work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,14 @@ import { DataService } from './services/data.service';
 import { FavoritesService } from './services/favorites.service';
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentsModule } from './components/components.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
@@ -26,4 +29,4 @@ import { ComponentsModule } from './components/components.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
